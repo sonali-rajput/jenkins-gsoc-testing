3c89f1e125a665f26eda731f400b86e3cf2519d7
Sonali Rajput <sonalirajput1088@gmail.com>
2023-03-18 17:00:09 +0000

    modify app.py

--- app.py
+++ app.py
@@ -5,12 +5,9 @@ result = subprocess.run(['docker','compose','logs'], stdout=subprocess.PIPE)
 data = result.stdout
 #read whole file to a string
 #data = text_file.read()
-print(data)
 #close file
 text_file.close()
 
 
 if data.find('failed to start containerd: timeout waiting for containerd to start') != -1:
     raise Exception("failed to start containerd: timeout waiting for containerd to start")
-
-raise Exception("error for testing")
