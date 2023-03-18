3552483bd132ee612a3b6383b843cd88d7fc96ef
Sonali Rajput <sonalirajput1088@gmail.com>
2023-03-18 16:21:20 +0000

    check for error

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ text_file = open("./logs.txt", "r")
 
 #read whole file to a string
 data = text_file.read()
-
+print(data)
 #close file
 text_file.close()
 
 
 if data.find('failed to start containerd: timeout waiting for containerd to start') != -1:
     raise Exception("failed to start containerd: timeout waiting for containerd to start")
+
+raise Exception("error for testing")
